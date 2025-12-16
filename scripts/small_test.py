0fd79559574ef1f4e0cc324ac05373080dd366ea
Jedrzej Stanislaw Slowinski <slowinsj@hu-berlin.de>
2025-12-16 07:50:28 +0100

    Refactor small_test.py to enhance logging and load development requirements

--- scripts/small_test.py
+++ scripts/small_test.py
@@ -1,5 +1,27 @@
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Try to load all requirements-dev.txt
+def load_requirements_dev():
+    import pkg_resources
+    import subprocess
+    import sys
+    try:
+        logger.info("Checking development requirements...")
+        with open('requirements-dev.txt') as f:
+            requirements = f.read().splitlines()
+        pkg_resources.require(requirements)
+    except pkg_resources.DistributionNotFound:
+        logger.info("Some development requirements are missing. Installing...")
+        # subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements-dev.txt'])
+
 def main():
-    print("This is a geometry utility script.")
-    
+    logger.info("This is a small test script for PyPSA Simplified European Model.")
+    load_requirements_dev()
+    logger.info("All development requirements are installed.")
+
 if __name__ == "__main__":
     main()
